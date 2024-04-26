9d847128b2cf49eb177c51a77cae125213d134ad
sundaysage <semenike60@gmail.com>
2024-04-26 13:36:08 -0700

    coo1

--- src/pages/deposit.js
+++ src/pages/deposit.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Deposit = () => {
     return (
         <div className='bg-blue-500 md:w-4/5 md:absolute right-0 bottom-0 md:h-h-t h-screen'>
-          <FontAwesomeIcon icon={faHouse} color='#FF0000'/>  Deposit
+           Deposit
         </div>
     )
 }
